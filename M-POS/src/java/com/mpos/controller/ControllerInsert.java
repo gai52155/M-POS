623f1e99822d362f3d66aeaa0f624fd839f3e21d
sasnostep <catza009@hotmail.com>
2016-06-06 16:42:45 +0700

    Updated InsertController

--- M-POS/src/java/com/mpos/controller/ControllerInsert.java
+++ M-POS/src/java/com/mpos/controller/ControllerInsert.java
@@ -6,8 +6,6 @@
 package com.mpos.controller;
 
 import com.mpos.model.ModelLocation;
-import java.util.Iterator;
-import java.util.List;
 import org.hibernate.HibernateException;
 import org.hibernate.SQLQuery;
 import org.hibernate.Session;
@@ -19,30 +17,15 @@ import org.hibernate.Transaction;
  * @author Windows 8
  */
 public class ControllerInsert {
-    public static void Insertor(){
+    public static void Insertor (){
     SessionFactory factory = HibernateUtil.getSessionFactory();
         Session session = factory.openSession();
         Transaction tx = null;
         try {
             tx = session.beginTransaction();
-            String sql = "INSERT INTO mpos_location_demo(name,lat,lng,iso,province,abbrevilation_id,country)"+
-                    "SELECT name,lat,lng,iso,province,abbrevilation_id,country FROM mpos_location_demo";
+            String sql = "INSERT INTO mpos_location_demo (name,lat,lng,iso,province,abbrevilation_id,country) VALUES ('qqq',11,11,'th','bangkok',1,'thailand')";
             SQLQuery query = session.createSQLQuery(sql);
-            query.addEntity(ModelLocation.class);
-            List location = query.list();
-            if (location.isEmpty()) {
-                System.out.println("No data");
-            }
-            for (Iterator iterator
-                    = location.iterator(); iterator.hasNext();) {
-                ModelLocation place = (ModelLocation) iterator.next();
-                System.out.print("ID : " + place.getlocation_id());
-                System.out.print("name : " + place.getname());
-                System.out.println("lat : " + place.getlat());
-                System.out.print("lng : " + place.getlng());
-                System.out.print("iso : " + place.getiso());
-                System.out.println("province : " + place.getprovince());
-            }
+            query.executeUpdate();
             tx.commit();
         } catch (HibernateException e) {
             if (tx != null) {
