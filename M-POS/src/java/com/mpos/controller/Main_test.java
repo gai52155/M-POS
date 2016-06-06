7cfd67594216c96f0bf8a53abbce519914f854c8
gai52155 <gai52155@outlook.com>
2016-06-06 15:30:54 +0700

    DELETOR COMPLETE

--- M-POS/src/java/com/mpos/controller/Main_test.java
+++ M-POS/src/java/com/mpos/controller/Main_test.java
@@ -6,5 +6,6 @@ package com.mpos.controller;
 public class Main_test {
     public static void main(String[] args) {
         ControllerSelect.selector();
+        //ControllerDelete.deletor(222);
     }
 }
