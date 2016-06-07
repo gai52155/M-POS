c292b6502240c9ca798b5a8061ad15a06b98de99
gai52155 <gai52155@outlook.com>
2016-06-07 15:25:56 +0700

    TRY MAKE PDF WITH ITEXT!

--- M-POS/src/java/com/mpos/controller/ControllerDelete.java
+++ M-POS/src/java/com/mpos/controller/ControllerDelete.java
@@ -1,26 +1,23 @@
 package com.mpos.controller;
 
-import java.util.Iterator;
-import java.util.List;
 import org.hibernate.HibernateException;
 import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import com.mpos.model.ModelLocation;
 
 /**
  *
  * @author Katawut
  */
 public class ControllerDelete {
-    public static void deletor(int id){
+    public static void deletor(String name){
     SessionFactory factory = HibernateUtil.getSessionFactory();
         Session session = factory.openSession();
         Transaction tx = null;
         try {
             tx = session.beginTransaction();
-            String sql = "DELETE FROM mpos_location_demo WHERE mposdemo_id = '"+id+"'";
+            String sql = "DELETE FROM location WHERE Name = '"+name+"'";
             SQLQuery query = session.createSQLQuery(sql);
             query.executeUpdate();
             tx.commit();
